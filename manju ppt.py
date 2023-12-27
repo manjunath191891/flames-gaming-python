83dfdb837a0e770d3d7664a1af65928a14f6d27e
manjunath191891 <155013592+manjunath191891@users.noreply.github.com>
2023-12-28 00:09:31 +0530

    Add files via upload

---
+++ manju ppt.py
@@ -0,0 +1,38 @@
+def Flames(name1,name2):
+    namestr = name1 + name2
+
+    for c in namestr:
+        if namestr.count(c) != 1:
+            
+            namestr = namestr.replace(c,"")
+
+    
+
+    print("FLAMES....")
+    print("F = Friend \nL = Love \nA = Affection \nM = Marriage \nE = Enemy \nS = Siblings \n\n")
+    
+
+    number = len(namestr) % 6
+    
+    rel = ""
+
+    if number == 1:
+        rel += "Friends"
+    elif number == 2:
+        rel += "Love"
+    elif number == 3:
+        rel += "Affection"
+    elif number == 4:
+        rel += "Marriage"
+    elif number == 5:
+        rel += "Enemy"
+    elif number == 0:
+        rel += "Siblings"
+    else:
+        pass
+
+    return rel
+
+n1 = input("Enter your name : ").upper()
+n2 = input("Enter name of your crush : ").upper()
+print(f"Your Relationship is : {Flames(n1,n2)}")
